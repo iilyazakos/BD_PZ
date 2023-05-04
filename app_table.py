e27d990f6f2be2784ca2a3f55ef14777c5aa5468
Ilya Nenartovich <iilyazakos@gmail.com>
2023-05-04 23:12:33 +0300

    Update app_table.py

--- app_table.py
+++ app_table.py
@@ -9,22 +9,18 @@ def open_link(url, new_tab=True):
     div = Div(text = html)
     st.bokeh_chart(div)
 
-
 data = pd.read_csv('https://github.com/iilyazakos/BD_PZ/blob/1984c0a0978133d4667c87880a1a4feb719c2922/data.txt?raw=true', sep=", ")
 st.set_page_config(layout = "wide")
 st.title('Table')
-
 s = 0
 for i in range(0, 7):
     s = data["tax"][i]*(data["cost(rub)"][i]*data["avg"][i])+s
-
 f = 0
 for i in range(0, 7):
   f = f+data["cost(rub)"][i]*data["avg"][i]
 
 data['avg_cost'] = data['avg'] * data['cost(rub)']
 data['price(rub)'] = round((data['avg_cost']*(1+data['tax'])+data['delivery(rub)']), 2)
-
 sums = pd.Series(["Сумма:", "", data["avg"].sum(),
                   f,
                   "", round(s, 2),
@@ -32,10 +28,6 @@ sums = pd.Series(["Сумма:", "", data["avg"].sum(),
                  "", ""],
                  index=data.columns)
 data = data.append(sums, ignore_index=True)
-
 finish = pd.Series(["Итог:", f+s+data["delivery(rub)"].sum(), " ", " ", " ", " ", " ", " ", " " ], index=data.columns)
-
 data = data.append(finish, ignore_index=True)
-
 st.write(data)
-
