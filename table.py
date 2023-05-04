0c5b47892b6750d896355465a33b69922f5f2585
Ilya Nenartovich <iilyazakos@gmail.com>
2023-05-05 00:23:27 +0300

    Update table.py

--- table.py
+++ table.py
@@ -1,16 +1,11 @@
 import streamlit as st
-from bokeh.models.widgets import Div
 import pandas as pd
+import webbrowser
 
-def open_link(url, new_tab=True):
-    if new_tab: js = f"window.open('{url}')"
-    else: js = f"window.location.href = '{url}'"
-    html = '<img src onerror="{}">'.format(js)
-    div = Div(text = html)
-    st.bokeh_chart(div)
-
-
-data = pd.read_csv('https://github.com/iilyazakos/BD_PZ/blob/1984c0a0978133d4667c87880a1a4feb719c2922/data.txt?raw=true', sep=", ")
+data = pd.read_csv('data.txt', sep=", ")
 st.set_page_config(layout = "wide")
 st.title('Table')
 st.write(data)
+
+if st.button('Table with calculated values'):
+    webbrowser.open_new_tab('https://iilyazakos-bd-pz-app-table-5sk2b1.streamlit.app/')
