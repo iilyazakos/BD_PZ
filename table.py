cf8a3338a6ecf9581c7a988d7b5dc1f988368c53
Ilya Nenartovich <iilyazakos@gmail.com>
2023-05-05 00:45:26 +0300

    Updaate table.py

--- table.py
+++ table.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import webbrowser
 
-data = pd.read_csv('data.txt', sep=", ")
+data = pd.read_csv('https://github.com/iilyazakos/BD_PZ/blob/1984c0a0978133d4667c87880a1a4feb719c2922/data.txt?raw=true', sep=", ")
 st.set_page_config(layout = "wide")
 st.title('Table')
 st.write(data)
