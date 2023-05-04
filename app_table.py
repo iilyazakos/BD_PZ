abca2f326fee588b91659d250ad344dcad305611
Ilya Nenartovich <iilyazakos@gmail.com>
2023-05-05 00:48:24 +0300

    Update app_table.py

--- app_table.py
+++ app_table.py
@@ -1,13 +1,6 @@
 import streamlit as st
-from bokeh.models.widgets import Div
 import pandas as pd
-
-def open_link(url, new_tab=True):
-    if new_tab: js = f"window.open('{url}')"
-    else: js = f"window.location.href = '{url}'"
-    html = '<img src onerror="{}">'.format(js)
-    div = Div(text = html)
-    st.bokeh_chart(div)
+import webbrowser
 
 data = pd.read_csv('https://github.com/iilyazakos/BD_PZ/blob/1984c0a0978133d4667c87880a1a4feb719c2922/data.txt?raw=true', sep=", ")
 st.set_page_config(layout = "wide")
