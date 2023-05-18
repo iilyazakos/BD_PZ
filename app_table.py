9d0e96ede1deb6518482295391e60caf73937433
Ilya Nenartovich <iilyazakos@gmail.com>
2023-05-18 05:17:27 +0300

    Update app_table.py
    fix

--- app_table.py
+++ app_table.py
@@ -4,7 +4,7 @@ from bokeh.models.widgets import Div
 
 data = pd.read_csv('https://github.com/iilyazakos/BD_PZ/blob/1984c0a0978133d4667c87880a1a4feb719c2922/data.txt?raw=true', sep=", ")
 st.set_page_config(layout = "wide")
-st.title('Table')
+st.title('Table with calculated values')
 s = 0
 for i in range(0, 7):
     s = data["tax"][i]*(data["cost(rub)"][i]*data["avg"][i])+s
@@ -14,11 +14,12 @@ for i in range(0, 7):
 
 data['avg_cost'] = data['avg'] * data['cost(rub)']
 data['price(rub)'] = round((data['avg_cost']*(1+data['tax'])+data['delivery(rub)']), 2)
-sums = pd.Series([str('Amount:'), "", data["avg"].sum(), f, "", round(s, 2), data["delivery(rub)"].sum(),"", ""], index=data.columns)
+sums = pd.DataFrame([[str('Amount:'), "", data["avg"].sum(), f, "", round(s, 2), data["delivery(rub)"].sum(),"", ""]], columns=data.columns)
 
-data = data.append(sums, ignore_index=True)
-finish = pd.Series([str('Result:'), round((f+s+data["delivery(rub)"].sum()), 2),"", "", "", "", "", "", ""], index=data.columns)
-data = data.append(finish, ignore_index=True)
+
+data = pd.concat([data, sums])
+finish = pd.DataFrame([[str('Result:'), round((f+s+data["delivery(rub)"].sum()), 2), "", "", "", "", "", "", ""]], columns=data.columns)
+data = pd.concat([data, finish], ignore_index=True)
 st.write(data)
 
 if st.button('Return to original page'):
